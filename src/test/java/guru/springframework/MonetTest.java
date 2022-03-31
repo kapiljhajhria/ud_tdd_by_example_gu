0ddeb4b597e3fa244e37e811399293310aba9af1
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-31 13:20:44 +0530

    another identical class created

--- src/test/java/guru/springframework/MonetTest.java
+++ src/test/java/guru/springframework/MonetTest.java
@@ -6,22 +6,43 @@ import org.junit.jupiter.api.Test;
 public class MonetTest {
 
     @Test
-    public void testMultiplication() {
+    public void testMultiplicationDollar() {
 
         Dollar five = new Dollar(5);
 
         Dollar product = five.times(2);
 
-        Assertions.assertEquals(10, product);
+        Assertions.assertEquals(new Dollar(10), product);
 
         product = five.times(3);
 
-        Assertions.assertEquals(15, product);
+        Assertions.assertEquals(new Dollar(15), product);
     }
 
     @Test
-    void testEquality() {
+    void testEqualityDollar() {
         Assertions.assertEquals(new Dollar(5),new Dollar(5));
         Assertions.assertNotEquals(new Dollar(5),new Dollar(6));
     }
+
+    @Test
+    public void testMultiplicationFranc() {
+
+        Franc five = new Franc(5);
+
+        Franc product = five.times(2);
+
+        Assertions.assertEquals(new Franc(10), product);
+
+        product = five.times(3);
+
+        Assertions.assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc() {
+        Assertions.assertEquals(new Franc(5),new Franc(5));
+        Assertions.assertNotEquals(new Franc(5),new Franc(6));
+    }
+
 }
