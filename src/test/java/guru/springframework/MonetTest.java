fa9ce9f7bf7b3ce9e7201e26f48109f81a9462a8
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-31 12:45:52 +0530

    Dollar Stub created to pass the test

---
+++ src/test/java/guru/springframework/MonetTest.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MonetTest {
+
+    @Test
+    public void testMultiplication() {
+
+        Dollar five = new Dollar(5);
+
+        five.times(2);
+
+        Assertions.assertEquals(10, five.amount);
+    }
+}
