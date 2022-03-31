5de81dded6edaf628ed53ca67690efd8ef2e416a
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-31 17:01:19 +0530

    Refactoring test cases into One Class tests

--- src/test/java/guru/springframework/MonetTest.java
+++ src/test/java/guru/springframework/MonetTest.java
@@ -6,36 +6,28 @@ import org.junit.jupiter.api.Test;
 public class MonetTest {
 
     @Test
-    public void testMultiplicationDollar() {
+    public void testMultiplication() {
 
-        Money five =  Money.dollar(5);
+        Money fiveD =  Money.dollar(5);
+        Money fiveF =  Money.franc(5);
 
-        Assertions.assertEquals(new Money(10,"USD"), five.times(2));
-        Assertions.assertEquals(new Money(15,"USD"), five.times(3));
+        Assertions.assertEquals(new Money(10,"USD"), fiveD.times(2));
+        Assertions.assertEquals(new Money(15,"USD"), fiveD.times(3));
+        Assertions.assertEquals(new Money(10,"CHF"), fiveF.times(2));
+        Assertions.assertEquals(new Money(15,"CHF"), fiveF.times(3));
     }
 
     @Test
-    void testEqualityDollar() {
-        Assertions.assertEquals(new Money(5,"USD"),new Money(5,"USD"));
-        Assertions.assertNotEquals(new Money(5,"USD"),new Money(6,"USD"));
+    void testEquality() {
+        Assertions.assertEquals( Money.dollar(5), Money.dollar(5));
+        Assertions.assertNotEquals( Money.dollar(5), Money.dollar(6));
+
+        Assertions.assertNotEquals(Money.franc(5), Money.franc(6));
+        Assertions.assertEquals(Money.franc(5), Money.franc(5));
+
+        Assertions.assertNotEquals( Money.franc(5), Money.dollar(5));
     }
 
-    @Test
-    public void testMultiplicationFranc() {
-
-        Money five =  Money.franc(5);
-
-        Assertions.assertEquals(new Money(10,"CHF"), five.times(2));
-
-        Assertions.assertEquals(new Money(15,"CHF"), five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc() {
-        Assertions.assertEquals(new Money(5,"CHF"),new Money(5,"CHF"));
-        Assertions.assertNotEquals(new Money(5,"CHF"),new Money(6,"CHF"));
-        Assertions.assertNotEquals(new Money(5,"USD"),new Money(5,"CHF"));
-    }
 
     @Test
     void testCurrency() {
