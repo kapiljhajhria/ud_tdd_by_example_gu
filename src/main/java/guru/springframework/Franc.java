43413a9e2bbc4d158b59619cf70f501f648bc88a
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-31 15:23:51 +0530

    create constructor in base class and use super() in other sub classes

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -3,8 +3,7 @@ package guru.springframework;
 public class Franc extends Money{
 
     public Franc(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
+        super(amount, currency);
     }
 
     public Franc times(int multiplier) {
